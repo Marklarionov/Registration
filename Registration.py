88267c176f1340c65f41a96dd1b1cdcf6f82973a
Marklarionov <93324243+Marklarionov@users.noreply.github.com>
2022-01-11 20:57:12 +0200

    Update Registration.py

--- Registration.py
+++ Registration.py
@@ -1,9 +1,10 @@
 import random
+from MyModule import *
 length=0
-login=[]
-psword=[]
+login=["m"]
+psword=["rrrrrr6R"]
 while 1:
-    print("Registration or Authorization?(1 or 2)")
+    print("Registration or Authorization or Exit?(1 or 2 or 3)")
     ans=int(input())
     if ans==1:
         log=input("Login: ")
@@ -14,4 +15,23 @@ while 1:
             print("You need minimum 6 characters, at least 1 number and 1 capital letter")
             print("Please,write down your password")
             pas=input()
-            print(pas_check)
+            print(pas_check)  
+            pas.join(psword)
+        elif ans1==2:
+            print("Password is being generated.")
+            print(ran_pass)
+            print("Remember this password.")
+        else:
+            print("Error")
+    elif ans==2:
+        print("Write your Login.")
+        log1=input("")
+        if log1 in login:
+            print("Write your password.")
+            pas1=input()
+            if pas1 in psword:
+                print("You are successfully logged in!")
+    elif ans==3:
+        print("Have a nice day!")
+    else:
+        print("Error")
