ea4aa27374584741d48407fbc405518183cb0cd2
Marklarionov <93324243+Marklarionov@users.noreply.github.com>
2022-01-11 20:57:36 +0200

    Update MyModule.py

--- MyModule.py
+++ MyModule.py
@@ -1,5 +1,5 @@
-import random
-def pass():
+from random import *
+def ran_pass():
     """The program will generate a random password.
     """
     str0=".,:;!_*-+()/#¤%&"
@@ -11,32 +11,25 @@ def pass():
     random.shuffle(ls)
     psword = ''.join([random.choice(ls) for x in range(12)])
     print(psword)
-#def pas_check(pas:str):
-#    """Write down your password and computer check his difficult.
-#    """
-#    for i in p:
-#        up=0
-#        low=0
-#        symb=0
-#        num=0 
-#        if i.isupper():
-#            up += 1
-#        elif i.islower():
-#            low += 1
-#        elif i.isdigit():
-#             num += 1
-#        else:
-#             symb += 1
-#    if len(pas) <= 6:
-#        print("Minimum 6 chracters!")
-#    elif up > 0 and low > 0 and symb > 0 and num > 0:
-#        print("Good password")
-#    else:
-#        print("To weak password")
-def pas_check(pas):
-return not(len(pas) < 10 or
-            pas.isdigit() or
-            pas.isalpha() or
-            pas.islower() or
-            pas.isupper()) \
-            and pas.isalnum()
+def pas_check():
+    """Write down your password and computer check his difficult.
+    """
+    for i in pas:
+        up=0
+        low=0
+        symb=0
+        num=0 
+        if i.isupper():
+            up += 1
+        elif i.islower():
+            low += 1
+        elif i.isdigit():
+             num += 1
+        else:
+             symb += 1
+    if len(pas) <= 6:
+        print("Minimum 6 chracters!")
+    elif up >= 0 and low >= 0 and symb >= 0 and num >= 0:
+        print("Good password")
+    else:
+        print("Too weak password")
