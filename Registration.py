77850ae637dd77f5b68c4835f4907eca648543d9
Marklarionov <93324243+Marklarionov@users.noreply.github.com>
2022-01-13 15:16:14 +0200

    Update Registration.py

--- Registration.py
+++ Registration.py
@@ -1,14 +1,14 @@
 from MyModule import*
 from random import*
-login=["m","lol"]
-psword=["rrrrrr6R","lollol6L"]
+login=[""]
+psword=[""]
 while 1:
     print("Registration or Authorization or Exit?(1 or 2 or 3)")
     ans=int(input())
     if ans==1:
-        print(registr())
+        print(registr(login,psword))
     elif ans==2:
-        print(log_in())
+        print(log_in(login,psword))
     elif ans==3:
         print("Have a nice day!")
         break
